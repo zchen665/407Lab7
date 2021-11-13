7294d3bfce1060ae45166cd6d8f4b9cf551d2a12
zchen665 <azchen665@gmail.com>
2021-11-13 13:40:28 -0600

    m1 complete

--- app/src/main/java/com/example/a407lab7/App.java
+++ app/src/main/java/com/example/a407lab7/App.java
@@ -1,10 +1,13 @@
 package com.example.a407lab7;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.Build;
 
 public class App extends Application {
     public final static String CH_1_ID = "channel1";
+    public final static String CH_2_ID = "channel2";
 
     @Override
     public void onCreate(){
@@ -13,7 +16,23 @@ public class App extends Application {
     }
 
     private void createNotificationChannels() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.0){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel ch1 = new NotificationChannel(
+                    CH_1_ID,
+                    "channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            NotificationChannel ch2 = new NotificationChannel(
+                    CH_2_ID,
+                    "channel 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            ch1.setDescription("This is channel 1");
+            ch2.setDescription("This is channel 2 hutao");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(ch1);
+            manager.createNotificationChannel(ch2);
 
         }
     }
--- app/src/main/java/com/example/a407lab7/MainActivity.java
+++ app/src/main/java/com/example/a407lab7/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.a407lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat nManager;
+    private EditText edit_title;
+    private EditText edit_message;
+    private String CH_1_ID = App.CH_1_ID;
+    private String CH_2_ID = App.CH_2_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nManager = NotificationManagerCompat.from(this);
+    }
+
+    public void sendOnChannel1(View v){
+        edit_title = (EditText) findViewById(R.id.text_title);
+        edit_message = (EditText) findViewById(R.id.text_message);
+        String title = edit_title.getText().toString();
+        String msg = edit_message.getText().toString();
+
+
+        Notification notification = new NotificationCompat.Builder(this, CH_1_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        nManager.notify(1,notification);
+    }
+    public void sendOnChannel2(View v){
+        edit_title = (EditText) findViewById(R.id.text_title);
+        edit_message = (EditText) findViewById(R.id.text_message);
+        String title = edit_title.getText().toString();
+        String msg = edit_message.getText().toString();
+
+
+        Notification notification = new NotificationCompat.Builder(this, CH_2_ID)
+                .setSmallIcon(R.drawable.hutao)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        nManager.notify(2,notification);
     }
 }
